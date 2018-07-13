eac82f1a4ff7cabe0405cf9d5892a4d32eb07faf
Tetsuya Mori <monry84@gmail.com>
2018-07-13 12:29:57 +0900

    :seedling: Initial commit

---
+++ Assets/Scripts/ZenjectAssert.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+
+namespace ExtraZenject
+{
+    // In order to avoid confusion with UnityEngine.Assertion and ModestTree, class names are dared to be redundant
+    public static class ZenjectAssert
+    {
+        [Conditional("UNITY_EDITOR")]
+        public static void IsInjected<T>(T value, string message = "")
+        {
+            if (value == null)
+            {
+                Fail(string.IsNullOrEmpty(message) ? $"`{typeof(T)}' seems to be not injected." : message, true);
+            }
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        private static void Fail(string message, bool useWarning = false)
+        {
+            if (useWarning)
+            {
+                Debug.LogWarning($"Assertion warning: {message}");
+            }
+            else
+            {
+                Debug.LogAssertion($"Assertion failed: {message}");
+            }
+        }
+    }
+}
