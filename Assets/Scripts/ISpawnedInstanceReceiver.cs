d7640bbc7a968d3163e94384feb392a7c30d587c
Tetsuya Mori <monry84@gmail.com>
2019-01-08 15:43:55 +0900

    :fire: Remove unnecessary log

--- Assets/Scripts/ISpawnedInstanceReceiver.cs
+++ Assets/Scripts/ISpawnedInstanceReceiver.cs
@@ -14,7 +14,6 @@ namespace ExtraZenject
     {
         public static IObservable<T> OnSpawnAsObservable<T>(this ISpawnedInstanceReceiver self)
         {
-            self.Receiver.GetStream<SpawnedInstance<T>>().Subscribe(_ => UnityEngine.Debug.Log(_));
             return self.Receiver.GetStream<SpawnedInstance<T>>().Select(x => x.Instance);
         }
     }
